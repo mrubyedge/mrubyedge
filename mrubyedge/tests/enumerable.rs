5085cc83464ec6b763930bac22947474eaf3feeb
Uchio Kondo <udzura@udzura.jp>
2026-02-02 19:00:57 +0900

    Add more tests

--- mrubyedge/tests/enumerable.rs
+++ mrubyedge/tests/enumerable.rs
@@ -159,3 +159,193 @@ fn enumerable_uniq_test() {
     let result_array: Vec<Rc<RObject>> = result.as_ref().try_into().unwrap();
     assert_eq!(result_array.len(), 3);
 }
+
+#[test]
+fn enumerable_select_test() {
+    let code = r#"
+    def test_select
+      [1, 2, 3, 4, 5].select { |x| x > 3 }
+    end
+    "#;
+    let binary = mrbc_compile("enumerable_select", code);
+    let mut rite = mrubyedge::rite::load(&binary).unwrap();
+    let mut vm = mrubyedge::yamrb::vm::VM::open(&mut rite);
+    vm.run().unwrap();
+
+    let args = vec![];
+    let result = mrb_funcall(&mut vm, None, "test_select", &args).unwrap();
+    let result_array: Vec<Rc<RObject>> = result.as_ref().try_into().unwrap();
+    assert_eq!(result_array.len(), 2);
+}
+
+#[test]
+fn enumerable_all_test() {
+    let code = r#"
+    def test_all
+      [2, 4, 6].all? { |x| x % 2 == 0 }
+    end
+    "#;
+    let binary = mrbc_compile("enumerable_all", code);
+    let mut rite = mrubyedge::rite::load(&binary).unwrap();
+    let mut vm = mrubyedge::yamrb::vm::VM::open(&mut rite);
+    vm.run().unwrap();
+
+    let args = vec![];
+    let result = mrb_funcall(&mut vm, None, "test_all", &args).unwrap();
+    let result: bool = result.as_ref().try_into().unwrap();
+    assert!(result);
+}
+
+#[test]
+fn enumerable_any_test() {
+    let code = r#"
+    def test_any
+      [1, 2, 3].any? { |x| x > 2 }
+    end
+    "#;
+    let binary = mrbc_compile("enumerable_any", code);
+    let mut rite = mrubyedge::rite::load(&binary).unwrap();
+    let mut vm = mrubyedge::yamrb::vm::VM::open(&mut rite);
+    vm.run().unwrap();
+
+    let args = vec![];
+    let result = mrb_funcall(&mut vm, None, "test_any", &args).unwrap();
+    let result: bool = result.as_ref().try_into().unwrap();
+    assert!(result);
+}
+
+#[test]
+fn enumerable_delete_if_test() {
+    let code = r#"
+    def test_delete_if
+      [1, 2, 3, 4, 5].delete_if { |x| x % 2 == 0 }
+    end
+    "#;
+    let binary = mrbc_compile("enumerable_delete_if", code);
+    let mut rite = mrubyedge::rite::load(&binary).unwrap();
+    let mut vm = mrubyedge::yamrb::vm::VM::open(&mut rite);
+    vm.run().unwrap();
+
+    let args = vec![];
+    let result = mrb_funcall(&mut vm, None, "test_delete_if", &args).unwrap();
+    let result_array: Vec<Rc<RObject>> = result.as_ref().try_into().unwrap();
+    assert_eq!(result_array.len(), 3);
+}
+
+#[test]
+fn enumerable_each_with_index_test() {
+    let code = r#"
+    def test_each_with_index
+      result = []
+      [10, 20, 30].each_with_index { |x, i| result.push(x + i) }
+      result
+    end
+    "#;
+    let binary = mrbc_compile("enumerable_each_with_index", code);
+    let mut rite = mrubyedge::rite::load(&binary).unwrap();
+    let mut vm = mrubyedge::yamrb::vm::VM::open(&mut rite);
+    vm.run().unwrap();
+
+    let args = vec![];
+    let result = mrb_funcall(&mut vm, None, "test_each_with_index", &args).unwrap();
+    let result_array: Vec<Rc<RObject>> = result.as_ref().try_into().unwrap();
+    assert_eq!(result_array.len(), 3);
+    let r0: i64 = result_array[0].as_ref().try_into().unwrap();
+    let r1: i64 = result_array[1].as_ref().try_into().unwrap();
+    let r2: i64 = result_array[2].as_ref().try_into().unwrap();
+    assert_eq!(r0, 10);
+    assert_eq!(r1, 21);
+    assert_eq!(r2, 32);
+}
+
+#[test]
+fn enumerable_sort_test() {
+    let code = r#"
+    def test_sort
+      [3, 1, 4, 1, 5].sort
+    end
+    "#;
+    let binary = mrbc_compile("enumerable_sort", code);
+    let mut rite = mrubyedge::rite::load(&binary).unwrap();
+    let mut vm = mrubyedge::yamrb::vm::VM::open(&mut rite);
+    vm.run().unwrap();
+
+    let args = vec![];
+    let result = mrb_funcall(&mut vm, None, "test_sort", &args).unwrap();
+    let result_array: Vec<Rc<RObject>> = result.as_ref().try_into().unwrap();
+    let r0: i64 = result_array[0].as_ref().try_into().unwrap();
+    assert_eq!(r0, 1);
+}
+
+#[test]
+fn enumerable_sort_by_test() {
+    let code = r#"
+    def test_sort_by
+      [3, 1, 4, 1, 5].sort_by { |x| -x }
+    end
+    "#;
+    let binary = mrbc_compile("enumerable_sort_by", code);
+    let mut rite = mrubyedge::rite::load(&binary).unwrap();
+    let mut vm = mrubyedge::yamrb::vm::VM::open(&mut rite);
+    vm.run().unwrap();
+
+    let args = vec![];
+    let result = mrb_funcall(&mut vm, None, "test_sort_by", &args).unwrap();
+    let result_array: Vec<Rc<RObject>> = result.as_ref().try_into().unwrap();
+    let r0: i64 = result_array[0].as_ref().try_into().unwrap();
+    assert_eq!(r0, 5);
+}
+
+#[test]
+fn enumerable_compact_test() {
+    let code = r#"
+    def test_compact
+      [1, nil, 2, nil, 3].compact
+    end
+    "#;
+    let binary = mrbc_compile("enumerable_compact", code);
+    let mut rite = mrubyedge::rite::load(&binary).unwrap();
+    let mut vm = mrubyedge::yamrb::vm::VM::open(&mut rite);
+    vm.run().unwrap();
+
+    let args = vec![];
+    let result = mrb_funcall(&mut vm, None, "test_compact", &args).unwrap();
+    let result_array: Vec<Rc<RObject>> = result.as_ref().try_into().unwrap();
+    assert_eq!(result_array.len(), 3);
+}
+
+#[test]
+fn enumerable_count_test() {
+    let code = r#"
+    def test_count
+      [1, 2, 3, 4, 5].count { |x| x % 2 == 0 }
+    end
+    "#;
+    let binary = mrbc_compile("enumerable_count", code);
+    let mut rite = mrubyedge::rite::load(&binary).unwrap();
+    let mut vm = mrubyedge::yamrb::vm::VM::open(&mut rite);
+    vm.run().unwrap();
+
+    let args = vec![];
+    let result = mrb_funcall(&mut vm, None, "test_count", &args).unwrap();
+    let result: i64 = result.as_ref().try_into().unwrap();
+    assert_eq!(result, 2);
+}
+
+#[test]
+fn enumerable_to_a_test() {
+    let code = r#"
+    def test_to_a
+      [1, 2, 3].to_a
+    end
+    "#;
+    let binary = mrbc_compile("enumerable_to_a", code);
+    let mut rite = mrubyedge::rite::load(&binary).unwrap();
+    let mut vm = mrubyedge::yamrb::vm::VM::open(&mut rite);
+    vm.run().unwrap();
+
+    let args = vec![];
+    let result = mrb_funcall(&mut vm, None, "test_to_a", &args).unwrap();
+    let result_array: Vec<Rc<RObject>> = result.as_ref().try_into().unwrap();
+    assert_eq!(result_array.len(), 3);
+}
